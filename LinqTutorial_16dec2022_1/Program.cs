86c0d842c47362e097f73b8d382fc6167904b951
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-16 07:03:31 +0100

    extension method

--- LinqTutorial_16dec2022_1/Program.cs
+++ LinqTutorial_16dec2022_1/Program.cs
@@ -1,5 +1,12 @@
 ﻿namespace LinqTutorial_16dec2022_1;
 
+public static class StringExtensions
+{
+    public static int GetCountOfLines(this string input)
+    {
+        return input.Split("\n").Length;
+    }
+}
 internal class Program
 {
     static void Main(string[] args)
@@ -8,5 +15,18 @@ internal class Program
         var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
 
         Console.WriteLine(string.Join(", ", wordsLongerThan2Letters));
+
+        var multilineString = @"
+""Please give me a ride on your back!”
+Said the Duck to the Kangaroo.
+“I woulds it quite still, and say nothing but‘ Quack’,
+The whole of the long day through!
+And we’d got otheDee,and the Jelly BoLee,
+Over the land ,and over the sea;
+Please take me avride!Odo!”
+Said the Duck to the Kangaroo.";
+
+        var countOfLines = multilineString.GetCountOfLines();
+        Console.WriteLine(countOfLines);
     }
 }
