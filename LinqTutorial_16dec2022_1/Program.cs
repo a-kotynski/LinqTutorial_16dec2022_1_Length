d51f696de9a4b4ac579b19ec2445790b518d1ad1
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-16 06:44:49 +0100

    Add project files.

---
+++ LinqTutorial_16dec2022_1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LinqTutorial_16dec2022_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var words = new List<string> { "a", "bb", "ccc", "dddd" };
+        var wordsLongerThan2Letters = words.Where(word => word.Length > 2);
+
+        Console.WriteLine(string.Join(", ", wordsLongerThan2Letters));
+    }
+}
